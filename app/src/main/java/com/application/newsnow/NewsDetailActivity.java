6e721b0ddf20a1977d3e041cd919dd4e2e96c93e
Aleksandr Moroz <morozaleksandriv@gmail.com>
2022-05-09 19:52:18 +0300

    add NewsDetailActivity.java (news layout, news_detail layout)

---
+++ app/src/main/java/com/application/newsnow/NewsDetailActivity.java
@@ -0,0 +1,46 @@
+package com.application.newsnow;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+        toolbar = findViewById(R.id.toolbar_news_detail);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_news_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.share_news_detail:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                startActivity(intent);
+                break;
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return true;
+    }
+}
