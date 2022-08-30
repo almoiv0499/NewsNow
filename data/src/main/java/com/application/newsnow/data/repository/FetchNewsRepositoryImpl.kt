177578db0a29cc87d233e5fe637c6a685697f38a
Testovich <morozaleksandriv@gmail.com>
2022-08-30 21:45:47 +0300

    removed usecases

--- data/src/main/java/com/application/newsnow/data/repository/FetchNewsRepositoryImpl.kt
+++ data/src/main/java/com/application/newsnow/data/repository/FetchNewsRepositoryImpl.kt
@@ -3,16 +3,9 @@ package com.application.newsnow.data.repository
 import com.application.newsnow.data.retrofit.Api
 import com.application.newsnow.domain.model.ListNews
 import com.application.newsnow.domain.repository.FetchNewsRepository
-import io.reactivex.rxjava3.core.Observable
-import retrofit2.Call
 
 class FetchNewsRepositoryImpl(private val api: Api) : FetchNewsRepository {
 
     override suspend fun fetchNews(): ListNews = api.getNewsForTopNewsScreen()
-
-    override fun fetchNewsByCategory(category: String): Call<ListNews> =
-        api.getNewsByCategory(category = category)
-
-    override fun fetchNewsForSearchScreen(): Observable<ListNews> = api.getNewsForSearchScreen()
 
 }
--- domain/src/main/java/com/application/newsnow/domain/repository/FetchNewsRepository.kt
+++ domain/src/main/java/com/application/newsnow/domain/repository/FetchNewsRepository.kt
@@ -1,15 +1,9 @@
 package com.application.newsnow.domain.repository
 
 import com.application.newsnow.domain.model.ListNews
-import io.reactivex.rxjava3.core.Observable
-import retrofit2.Call
 
 interface FetchNewsRepository {
 
     suspend fun fetchNews(): ListNews
-
-    fun fetchNewsByCategory(category: String): Call<ListNews>
-
-    fun fetchNewsForSearchScreen(): Observable<ListNews>
 
 }
